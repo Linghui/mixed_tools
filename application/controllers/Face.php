58ae2ed23664a679db6dc36eb968c7c1b761e4d7
李英辉 <xiaohuidexinge@163.com>
2018-04-24 01:13:06 +0800

    no message

--- application/controllers/Face.php
+++ application/controllers/Face.php
@@ -15,6 +15,7 @@ class Face extends CI_Controller
             'api_secret' => $this->secret,
             'image_url' => 'http://oracle.pagecp.com/img/tou.jpg',
         );
+        $this->load->model('Curl_model');
         $res = $this->Curl_model->curl_post($url, $param);
         echo json_encode($res);
     }
