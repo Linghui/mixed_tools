608ae6ff086a3e1fe6246c65a75c5d38e17f70eb
李英辉 <xiaohuidexinge@163.com>
2018-04-24 01:20:40 +0800

    no message

--- application/controllers/Face.php
+++ application/controllers/Face.php
@@ -4,15 +4,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Face extends CI_Controller
 {
-    public $key = 'hW4M6KzZ7cRsilSImHylXpX93kTNWB7V';
-    public $secret = 'hvDGckrQKQgDzFdMFcBA0LivPq3RxpXW';
+    public $key = 'qoHuLwbh8-02hliI28d3HjGUVjLZUYOm';
+    public $secret = 'RVD_niLlp-Xco4qmKm0SJeBZ9Zxw1K9C';
     public function detect()
     {
         $url = 'https://api-cn.faceplusplus.com/facepp/v3/detect';
 
         $param = array(
-            // 'api_key' => $this->key,
-            // 'api_secret' => $this->secret,
+            'api_key' => $this->key,
+            'api_secret' => $this->secret,
             'image_url' => 'http://oracle.pagecp.com/img/tou.jpg',
         );
         echo json_encode($param);
